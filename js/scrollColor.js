f135905d86a5dc8ab7a136d9e339326cfde644da
S5ar <31011612+S5ar@users.noreply.github.com>
2019-01-22 17:10:48 +0530

    initial commit

---
+++ js/scrollColor.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 400 && scroll < 1000) {
+	    $(".fab").css("color" , "#000");
+      $(".fab").css("transition" , ".3s");
+	  }
+
+	  else{
+		  $(".fab").css("color" , "#fff");
+	  }
+  })
+});
+$(document).ready(function(){
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 100) {
+	    $("nav").css("background" , "rgba(103, 58, 183,.99)");
+      $("nav").css("box-shadow" , "2px 5px 5px -6px #000");
+      $("nav").css("transition" , ".3s");
+	  }
+
+	  else{
+		  $("nav").css("background-color" , "#000");
+      $("nav").css("box-shadow" , "none");
+
+	  }
+  })
+})
